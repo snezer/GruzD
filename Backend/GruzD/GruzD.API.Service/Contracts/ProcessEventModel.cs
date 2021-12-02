9c3fb72fca7c390038301ced741f712fcdf0ff6d
gribkovav <gribkovav@gmail.com>
2021-12-03 02:17:44 +0300

    publish events controller

---
+++ Backend/GruzD/GruzD.API.Service/Contracts/ProcessEventModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GruzD.Web.Contracts
+{
+    public class ProcessEventModel
+    {
+    }
+}
---
+++ Backend/GruzD/GruzD.API.Service/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GruzD.DAL.PgSql;
+using GruzD.Web.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GruzD.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private LogicDataContext _context;
+
+        public EventsController(LogicDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Published(ProcessEventModel model)
+        {
+            return Ok();
+        }
+    }
+}
