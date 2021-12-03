97b2cbc2e85092756156de201b020b3f0bf98d04
gribkovav <gribkovav@gmail.com>
2021-12-03 09:47:28 +0300

    Добавление событий в систему

--- Backend/GruzD/GruzD.API.Service/Contracts/ProcessEventModel.cs
+++ Backend/GruzD/GruzD.API.Service/Contracts/ProcessEventModel.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GruzD.DataModel.Events;
 
 namespace GruzD.Web.Contracts
 {
+    /// <summary>
+    /// Модель события на площадке
+    /// </summary>
     public class ProcessEventModel
     {
+        /// <summary>
+        /// Файлы-картинки, которые были сфотографированы в контексте события процесса
+        /// </summary>
+        public string[] Base64Pics { get; set; }
+
+        /// <summary>
+        /// Номер транспортного средства, если было распознано
+        /// </summary>
+        public string Number { get; set; }
+        /// <summary>
+        /// Дата возбуждения событьия
+        /// </summary>
+        public DateTime Occured { get; set; }
+        /// <summary>
+        /// Дополнительные сериализованные данные
+        /// </summary>
+        public string AuxData { get; set; }
+        /// <summary>
+        /// Тип события
+        /// </summary>
+        public virtual ProcessEventType ClassifiedType { get; set; }
+        /// <summary>
+        /// Вес, если относится к событию
+        /// </summary>
+        public decimal? Weight { get; set; }
     }
 }
--- Backend/GruzD/GruzD.API.Service/Controllers/EventsController.cs
+++ Backend/GruzD/GruzD.API.Service/Controllers/EventsController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using GruzD.DAL.PgSql;
+using GruzD.DataModel.Events;
 using GruzD.Web.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace GruzD.Web.Controllers
 {
@@ -13,16 +17,50 @@ namespace GruzD.Web.Controllers
     public class EventsController : ControllerBase
     {
         private LogicDataContext _context;
+        private IMapper _mapper;
 
-        public EventsController(LogicDataContext context)
+        public EventsController(LogicDataContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
+
+        /// <summary>
+        /// Передача события процесса
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost]
-        public IActionResult Published(ProcessEventModel model)
+        public async Task<IActionResult> Published(ProcessEventModel model)
         {
+            BLOBData[] blobs = GetBlobs(model.Base64Pics);
+            var dEvent = _mapper.Map<ProcessEvent>(model);
+            await _context.ProcessEvents.AddAsync(dEvent);
+            foreach (var blobData in blobs)
+            {
+                blobData.ProcessEvent = dEvent;
+                _context.Blobs.Add(blobData);
+            }
+
+            await _context.SaveChangesAsync();
             return Ok();
+        }
+
+        private BLOBData[] GetBlobs(string[] modelBase64Pics)
+        {
+            var res = modelBase64Pics.Select(s =>
+            {
+                var bytes = Convert.FromBase64String(s);
+                BLOBData d = new BLOBData()
+                {
+                    Content = bytes,
+                    ContentType = "application/jpeg",
+                };
+                return d;
+            }).ToArray();
+
+            return res;
         }
     }
 }
