84c4da2ad93946eccbe6c3fb0aff34b2f9afb972
kreolz <kostenko@rb.asu.ru>
2021-12-04 05:23:17 +0700

    добавил отображение списка событий у площадки

--- Frontend/src/pages/acceptance/Acceptance.vue
+++ Frontend/src/pages/acceptance/Acceptance.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <AcceptanceGalleria :photos-product="photosProduct" />
+    <AcceptanceGalleria :photos-product="photos" />
     <div class="p-grid">
       <div class="p-col-12">
-        Результат
+        Тип события: {{eventTypeWord[event.ClassifiedType]}}
       </div>
       <div class="p-col-12">
-        Дата {{new Date(acceptance.date).toLocaleDateString('ru', {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit"})}}
+        Дата возникновения события: {{calcDate(event.Occured)}}
       </div>
       <div class="p-col-12">
-        Обнаруженно {{acceptance.defective}} брака
+        Событие обработано: {{event.Process?'Да':'Нет'}}
+      </div>
+      <div class="p-col-12">
+        Дата обработки события: {{calcDate(event.ProcessTime)}}
+      </div>
+      <div class="p-col-12">
+      Вес: {{event.Weight}} тонн
       </div>
     </div>
     <div class="p-grid" style="margin-top: 10px">
@@ -23,26 +29,43 @@
 <script>
 import { Component, Vue} from "vue-property-decorator";
 import AcceptanceGalleria from "@/components/acceptance/AcceptanceGalleria";
+import { EventTypeWord } from "@/models/enum/EventType";
 @Component({
   components: { AcceptanceGalleria },
   props:{
-    acceptance: {
+    event: {
       type: Object,
       required: true,
     }
   }
 })
 export default class Acceptance extends Vue {
+  eventTypeWord = EventTypeWord
+  calcDate(date){
+    if (!date) return '-'
+    return new Date(date).toLocaleDateString('ru-Ru', {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit"})
+  }
+  get photos() {
+    try{
+      return this?.event.Base64PicsIds.map( photo=>{
+        return {
+          itemImageSrc: `/api/v1/File/${photo}`,
+          thumbnailImageSrc: `/api/v1/File/${photo}`,
+          alt: "Description for Image 1",
+          title: "Title 1"
+        }
+      })
+    }catch (e) {
+      return [
+        {
+          itemImageSrc: 'https://s0.rbk.ru/v6_top_pics/ampresize/media/img/9/44/755283020588449.jpg',
+          thumbnailImageSrc: 'https://s0.rbk.ru/v6_top_pics/ampresize/media/img/9/44/755283020588449.jpg',
+          alt: "Описание события",
+          title: 'Заголовок'
+        }
+      ]
+    }
 
-  get photosProduct() {
-    return this?.acceptance.photos.map( photo=>{
-      return {
-        itemImageSrc: photo,
-        thumbnailImageSrc: photo,
-        alt: "Description for Image 1",
-        title: "Title 1"
-      }
-    })
   }
 };
 </script>
