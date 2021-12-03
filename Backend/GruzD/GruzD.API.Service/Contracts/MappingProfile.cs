eb21d9fdcd9b0c50186bfa3abe6cbcaef81ddc65
gribkovav <gribkovav@gmail.com>
2021-12-03 12:34:31 +0300

    mapping

--- Backend/GruzD/GruzD.API.Service/Contracts/MappingProfile.cs
+++ Backend/GruzD/GruzD.API.Service/Contracts/MappingProfile.cs
@@ -1,7 +1,9 @@
+using System;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using GruzD.DataModel;
 using GruzD.DataModel.BL;
+using GruzD.DataModel.Events;
 using GruzD.Web.Contracts.User;
 using GruzD.Web.Contracts.Zone;
 using GruzD.Web.Models;
@@ -25,6 +27,14 @@ namespace GruzD.Web.Contracts
 
             CreateMap<UnloadingZone, ZoneModel>()
                 .ForSourceMember(e => e.Sources, o => o.DoNotValidate());
+
+            CreateMap<ProcessEventModel, ProcessEvent>()
+                .ForMember(t => t.Sources, e => e.Ignore())
+                .ForMember(t => t.ProcessTime, e => e.Ignore())
+                .ForMember(t => t.Processed, e => e.Ignore())
+                .ForMember(t => t.Registered, e => e.MapFrom(c => DateTime.Now));
+
+            CreateMap<ProcessEvent, ProcessEventModel>();
         }
     }
 }
