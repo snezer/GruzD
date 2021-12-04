d61a1521e4e793ecdf34d81911b7e969d10202d2
kreolz <kostenko@rb.asu.ru>
2021-12-04 10:29:06 +0700

    поправил поставщиков и поставки

--- Frontend/src/pages/delivery/Delivery.vue
+++ Frontend/src/pages/delivery/Delivery.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <DataTable :value="deliverys">
-      <Column field="ProviderName" header="Наименование поставщика"></Column>
-      <Column field="RawMaterialName" header="Материал поставки"></Column>
+      <Column field="ProviderId" header="Наименование поставщика">
+        <template #body="slotProps">
+          {{getNameProvider(slotProps.data.ProviderId)}}
+        </template>
+      </Column>
+      <Column field="RawMaterialName" header="Материал поставки">
+        <template #body>
+          Гречка высшего сорта
+        </template>
+      </Column>
       <Column field="PlannedDate" header="Плановая дата поставки">
         <template #body="slotProps">
           {{calcDate(slotProps.data.PlannedDate)}}
@@ -21,6 +29,9 @@
 import {Component, Vue} from "vue-property-decorator";
 @Component({})
 export default class Delivery extends Vue{
+  getNameProvider(ProviderId){
+    return this.$store.state.providers.providers.find(pr=>pr.Id==ProviderId).Name
+  }
   calcDate(date){
     return new Date(date).toLocaleDateString('ru-Ru', {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit"})
   }
@@ -30,6 +41,7 @@ export default class Delivery extends Vue{
 
   created(){
     this.$store.dispatch('delivery/get_deliverys')
+    this.$store.dispatch('providers/get_providers')
   }
 };
 </script>
